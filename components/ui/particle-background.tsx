db8f8c93cb847053f70843e83f4abd84255371dc
Doppy258 <lucaszhao09@gmail.com>
2025-10-05 16:46:19 -0400

    Fix TypeScript error: remove deprecated area property from density config

--- components/ui/particle-background.tsx
+++ components/ui/particle-background.tsx
@@ -79,7 +79,6 @@ export function ParticleBackground({ className = "" }: ParticleBackgroundProps) 
           number: {
             density: {
               enable: true,
-              area: 800,
             },
             value: 80,
           },
